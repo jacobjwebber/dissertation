a415d84fd9a5f8324eaffba493b6827e8f1a2831
Jacob Webber <s1116548@ness-gpu-1.eca.ed.ac.uk>
2017-06-10 21:10:12 +0100

    small changes

--- cuda_semi-structured/sphere.cu
+++ cuda_semi-structured/sphere.cu
@@ -20,19 +20,19 @@
 #define R 64/2
 
 struct block {
-    int up;
+    int up; //z direction
     int down;
-    int left;
+    int left; //x direction
     int right;
-    int fore;
+    int fore; //y direction.
     int aft;
     float u[Bl][Bm][Bp];
     float u1[Bl][Bm][Bp];
-    char inside[Bl][Bm][Bp];
+    char k[Bl][Bm][Bp];
     int coords[3];
 };
 
-__global__ void perform_stencil_internal(struct block *u, struct block *u1, real scale);
+__global__ void perform_stencil_internal(struct block *u, struct block *u1, real l2, real l, real g);
 
 
 char is_block_in(int x, int y, int z, char is_in_sphere[L][M][P]) {
@@ -49,6 +49,18 @@ char is_block_in(int x, int y, int z, char is_in_sphere[L][M][P]) {
 }
 
 int main() {
+
+    //Coefficients
+    //courant number and courant number squared.
+    real l2 = 1.0/3.0;
+    real l = sqrt(l2);
+    //Wall reflection coefficient.
+    real r = 0.9;
+    real g = (1-r)/(1+r);
+    //grid spacing (m)
+    real h = 0.1;
+    //Speed of sound
+    real c = 343;
 
     //Error checking - Bounding grid must be divisible by block.
     if(L%Bl != 0 || M%Bm != 0 || P%Bp != 0) {
@@ -76,6 +88,7 @@ int main() {
         printf("\n");
     }
     */
+    
 
     //Assign a block for all volumes containing points
     //by first assigning enough blocks for the whole lot.
@@ -83,6 +96,9 @@ int main() {
     int num_blocks_l = L/Bl;
     int num_blocks_m = M/Bm;
     int num_blocks_p = P/Bp;
+
+    //Create an array storing the location of each block.
+    int *index_of_struct = (int*) calloc(num_blocks_l*num_blocks_m*num_blocks_p, sizeof(int));
 
     struct block *array = (struct block *) calloc(num_blocks_l*num_blocks_m*num_blocks_p, sizeof(struct block));
     int blocks_in = 0;
@@ -97,20 +113,67 @@ int main() {
 }
 
 
-__global__ void perform_stencil_internal(struct block *u, struct block *u1, real scale) {
+__global__ void perform_stencil_internal(struct block *u, real l2, real l, real g) {
     //Do stencil.
     //internal elements
+
+    //The following statement effectively caches the block.
+    //FIX THIS using SHARED.???
     struct block bl = u[blockIdx.x];
     
     int x = threadIdx.x + 1;
     int y = threadIdx.y + 1;
     int z = threadIdx.z + 1;
+    int k = bl.k[x][y][z];
+
+    bl.u[x][y][z] = ((2 - l2 * k) * bl.u1[x][y][z] +
+                    l2 * ( bl.u1[x][y][z+1] + 
+                           bl.u1[x][y][z-1] + 
+                           bl.u1[x][y+1][z] + 
+                           bl.u1[x][y-1][z] + 
+                           bl.u1[x+1][y][z] +
+                           bl.u1[x-1][y][z] ) +
+                           (0.5 * l * g * (6 - k) - 1) * bl.u[x][y][z])/(1 + 0.5 * l * g * (6 - k));
+
+    if (k == 0) {
+        bl.u[x][y][z] = 0;
+    }
     
-    bl.u[x][y][z] = scale*( bl.u1[x][y][z+1] + 
-                            bl.u1[x][y][z-1] + 
-                            bl.u1[x][y+1][z] + 
-                            bl.u1[x][y-1][z] + 
-                            bl.u1[x+1][y][z] +
-                            bl.u1[x-1][y][z]) - bl.u[x][y][z];
+}
+__global__ void perform_stencil_surfaces_LR(struct block *u, real l2, real l, real g) {
+    //Do stencil.
+    //Edge elements left and right.
+    struct block bl = u[blockIdx.x];
+    struct block bl_r = u[bl.right];
+    struct block bl_l = u[bl.left];
+    
+    real left, right;
+
+    int y = threadIdx.y + 1;
+    int z = threadIdx.z + 1;
+    
+    if ( bl.left != -1 ) {
+        left = u[bl.left].u1[Bl-1][y][z];
+    } else {
+        left = 0;
+    }
+    
+    int k = bl.k[0][y][z];
+    
+    
+    bl.u[0][y][z] = ((2 - l2 * k) * bl.u1[0][y][z] +
+                    l2 * ( bl.u1[0][y][z+1] + 
+                           bl.u1[0][y][z-1] + 
+                           bl.u1[0][y+1][z] + 
+                           bl.u1[0][y-1][z] + 
+                           bl.u1[0+1][y][z] +
+                           left ) +
+                           (0.5 * l * g * (6 - k) - 1) * bl.u[0][y][z])/(1 + 0.5 * l * g * (6 - k));
+    
+   
+
+    if (k == 0) {
+        bl.u[0][y][z] = 0;
+    }
     
 }
