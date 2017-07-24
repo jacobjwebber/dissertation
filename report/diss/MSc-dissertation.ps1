143f98a331b8dd875acd69d03e3afe8f698343be
Jacob Webber <s1116548@enna.inf.ed.ac.uk>
2017-07-24 20:40:05 +0100

    got graph in report now i am dead

---
+++ report/diss/MSc-dissertation.ps1
@@ -0,0 +1,136 @@
+%!PS-Adobe-3.0 EPSF-3.0
+%%BoundingBox: (atend)
+%%Title: TeXdraw drawing: MSc-dissertation.ps1
+%%Pages: 1
+%%Creator: 1995/12/19 TeXdraw V2R0
+%%CreationDate: 2017/7/24
+50 dict begin
+/mv {stroke moveto} def
+/lv {lineto} def
+/st {currentpoint stroke moveto} def
+/sl {st setlinewidth} def
+/sd {st 0 setdash} def
+/sg {st setgray} def
+/bs {gsave} def /es {stroke grestore} def
+/fl {gsave setgray fill grestore
+ currentpoint newpath moveto} def
+/fp {gsave setgray fill grestore st} def
+/cv {curveto} def
+/cr {gsave currentpoint newpath 3 -1 roll 0 360 arc
+ stroke grestore} def
+/fc {gsave setgray currentpoint newpath
+ 3 -1 roll 0 360 arc fill grestore} def
+/ar {gsave currentpoint newpath 5 2 roll arc stroke grestore} def
+/el {gsave /svm matrix currentmatrix def
+ currentpoint translate scale newpath 0 0 1 0 360 arc
+ svm setmatrix stroke grestore} def
+/fe {gsave setgray currentpoint translate scale newpath
+ 0 0 1 0 360 arc fill grestore} def
+/av {/hhwid exch 2 div def /hlen exch def
+ /ah exch def /tipy exch def /tipx exch def
+ currentpoint /taily exch def /tailx exch def
+ /dx tipx tailx sub def /dy tipy taily sub def
+ /alen dx dx mul dy dy mul add sqrt def
+ /blen alen hlen sub def
+ gsave tailx taily translate dy dx atan rotate
+ (V) ah ne {blen 0 gt {blen 0 lineto} if} {alen 0 lineto} ifelse
+ stroke blen hhwid neg moveto alen 0 lineto blen hhwid lineto
+ (T) ah eq {closepath} if
+ (W) ah eq {gsave 1 setgray fill grestore closepath} if
+ (F) ah eq {fill} {stroke} ifelse
+ grestore tipx tipy moveto} def
+0.24 0.24 scale
+1 setlinecap 1 setlinejoin
+3 setlinewidth [] 0 setdash
+0 0 moveto
+3 sl
+4 sl
+209 143 mv
+229 143 lv
+1428 143 mv
+1408 143 lv
+209 208 mv
+229 208 lv
+1428 208 mv
+1408 208 lv
+209 272 mv
+229 272 lv
+1428 272 mv
+1408 272 lv
+209 337 mv
+229 337 lv
+1428 337 mv
+1408 337 lv
+209 401 mv
+229 401 lv
+1428 401 mv
+1408 401 lv
+209 465 mv
+229 465 lv
+1428 465 mv
+1408 465 lv
+209 530 mv
+229 530 lv
+1428 530 mv
+1408 530 lv
+209 594 mv
+229 594 lv
+1428 594 mv
+1408 594 lv
+209 659 mv
+229 659 lv
+1428 659 mv
+1408 659 lv
+209 723 mv
+229 723 lv
+1428 723 mv
+1408 723 lv
+209 787 mv
+229 787 lv
+1428 787 mv
+1408 787 lv
+209 852 mv
+229 852 lv
+1428 852 mv
+1408 852 lv
+209 143 mv
+209 163 lv
+209 852 mv
+209 832 lv
+362 143 mv
+362 163 lv
+362 852 mv
+362 832 lv
+514 143 mv
+514 163 lv
+514 852 mv
+514 832 lv
+666 143 mv
+666 163 lv
+666 852 mv
+666 832 lv
+819 143 mv
+819 163 lv
+819 852 mv
+819 832 lv
+970 143 mv
+970 163 lv
+970 852 mv
+970 832 lv
+1123 143 mv
+1123 163 lv
+1123 852 mv
+1123 832 lv
+1275 143 mv
+1275 163 lv
+1275 852 mv
+1275 832 lv
+1428 143 mv
+1428 163 lv
+1428 852 mv
+1428 832 lv
+209 852 mv
+209 143 lv
+1428 143 lv
+1428 852 lv
+209 852 lv
--- semi-structured/results/scripts/mem-cap_graph.gp
+++ semi-structured/results/scripts/mem-cap_graph.gp
@@ -1,8 +1,8 @@
-set terminal tex
+set terminal epslatex input color
 set output "../../../report/diagrams/mem-cap_sphere.tex"
 set xlabel "Diameter (Grid Points)"
 set autoscale
-set ylabel "Percentage (%)"
+set ylabel "$\\frac{S_{ss}}{S_{s}}$"
 set style data linespoints
 
-plot "../data/memory-cap_graph" with lines lc rgb "blue" notitle, "../data/memory-cap_graph" lc rgb "blue" notitle
+plot "../data/memory-cap_graph" with lines lc rgb "blue" notitle, "../data/memory-cap_graph" lc rgb "blue" notitle, pi/6 lt 2 lc rgb "red" notitle
