304fe5ed5f58d372f6b777e2380be5b602b5d56a
Jacob Webber <s1116548@ness-gpu-1.eca.ed.ac.uk>
2017-08-10 20:03:21 +0100

    aded cross code

--- semi-structured/memory-capacity/memory-capacity.cu
+++ semi-structured/memory-capacity/memory-capacity.cu
@@ -19,13 +19,15 @@ int main() {
     int diam = ceil(radius/h)+2;
     int X, Y, Z;
 
+    diam = 8;
+   
     // BEGIN DATA PREP SECTION
     struct block *aos;
     int blocks_in;
     int *index_of_struct;
     ss_t data;
     char* k_arr;
-    printf("Memory Capacity Experiment\n\n");
+/*    printf("Memory Capacity Experiment\n\n");
     printf("SPHERE\n");
     printf("Diam \tInternal Blocks\tSize semi-Structured (MiB) \tSize - Structured (MiB) \tRatio\n");
 
@@ -59,7 +61,24 @@ int main() {
         //end data prep
         //Set input and output locations
     }
-   
+*/ 
+    printf("\nCROSS\n");
+    printf("Diam \tInternal Blocks\tSize semi-Structured (MiB) \tSize - Structured (MiB) \tRatio\n");
+    for (diam = 8; diam <= 500; diam +=40) {
+        k_arr = make_cross(diam);
+        Z = diam;
+        X = Y = 3*diam;
+        // BEGIN DATA PREP SECTION
+        data = create_aos(X, Y, Z, k_arr, &blocks_in, &aos, &index_of_struct);
+        size_t bl_siz = ((blocks_in-1)*sizeof(struct block));
+        size_t s_siz = (X*Y*Z*(2*sizeof(real) + sizeof(char))) ;
+        float ratio = ((float) bl_siz) / ((float) s_siz);
+        printf("%i \t%i \t%f \t%f \t%f \n", diam, blocks_in-1, (float) bl_siz/(float)(1024*1024),(float) s_siz/ (float)(1024*1024), ratio);
+        free_ss(data);
+        //end data prep
+        //Set input and output locations
+    }
+  
     return 0;
 }
 
